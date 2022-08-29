c3398a87567f4d4ade7c01e3c49e20213c9aa731
Amit Ashdot <amitashdot@gmail.com>
2022-08-29 15:34:45 +0300

    small css changes

--- mail.php
+++ mail.php
@@ -4,21 +4,24 @@ $recepient = "amitashdot@gmail.com,";
 // $recepient = "eyalivne@gmail.com,micha@targetcall.co.il,"; 
 // $recepient = "amitashdot@gmail.com"; 
 
-$text_1 = trim($_POST["name"]);
-$text_2 = trim($_POST["phone"]);
-$text_3 = trim($_POST["email"]);
-$text_4 = trim($_POST["buss"]);
-$text_5 = trim($_POST["area"]);
+$Fname = trim($_POST["Fname"]);
+$Sname = trim($_POST["Sname"]);
+$id = trim($_POST["id"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$time = trim($_POST["time"]);
+$city = trim($_POST["city"]);
 
 $text_3 = trim($_POST["utm_campaign"]);
 $text_4 = trim($_POST["keywordBottom"]);
 
 $message = " \n ליד חדש מקמפיין סלולר - מקאן \n
-שם: $text_1 \n
-טלפון: $text_2\n
-אימייל: $text_3 \n
-תחום: $text_3 \n
-איזור: $text_3 \n
+שם: $Fname $Sname \n
+טלפון: $phone\n
+תעודת זהות: $id \n
+אימייל: $email \n
+שעה נוחה לחזור: $time \n
+עיר: $city \n
 
 קמפיין: $text_3 \n
 מילת מפתח : $text_4\n
